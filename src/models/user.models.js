91a4b52ec125216c18f6fd99913f1169275dc4eb
samir7-7 <samirnepal9817@gmail.com>
2026-06-19 22:50:29 +0545

    First Commit

---
+++ src/models/user.models.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Full name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "Email already exists"],
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.on("sa")
+
+export const User = mongoose.model("User", userSchema);
